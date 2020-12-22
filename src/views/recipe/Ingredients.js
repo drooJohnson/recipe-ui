e4b87f583e68289af1b654258a3a4986ee9467ec
Andrew Johnson <andrew.johnson@thynkhealth.com>
2020-12-22 14:32:31 -0600

    ingredient list styled

--- src/views/recipe/Ingredients.js
+++ src/views/recipe/Ingredients.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types'
 import MDEditor from '@uiw/react-md-editor'
+import styled from 'styled-components'
+
+const IngredientsStyles = styled(MDEditor.Markdown)`
+  font-family:'inherit';
+  ul,ol,dl{
+    list-style:none;
+    padding:0;
+  }
+  h3{
+    margin-top:16px;
+    margin-bottom:8px;
+  }
+`
 
 const Ingredients = ({ingredients}) => {
   return(
-    <MDEditor.Markdown style={{fontFamily:'inherit'}} source={ingredients}/>
+    <IngredientsStyles style={{fontFamily:'inherit'}} source={ingredients}/>
   )
 }
 
