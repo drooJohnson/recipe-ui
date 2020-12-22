4fd490fb6b37d874f96ddcf93ea948ffeb90b250
Andrew Johnson <andrew.johnson@thynkhealth.com>
2020-12-22 14:55:23 -0600

    Recipe list looks better

--- src/views/components/RecipeCard.js
+++ src/views/components/RecipeCard.js
@@ -1,16 +1,46 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
+import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
+import FilteredImage from './FilteredImage';
+import styled from 'styled-components';
+import {device} from '../../utils/device';
+import TextTruncate from 'react-text-truncate';
+
 import LinkTagList from './LinkTagList';
 
-import {Link} from 'react-router-dom';
+import {useHistory} from 'react-router-dom';
+
+const Recipe = styled.div`
+
+`
+const Image = styled(CardMedia)`
+  height:180px;
+`
 
 const RecipeCard = ({recipe}) => {
+  const history = useHistory();
   return(
+    <Card style={{flexGrow:1}}>
+      <CardActionArea onClick={()=>{history.push(`/recipe/${recipe.id}`)}}>
+      <Image title={recipe.name} image={recipe.imageUrl}/>
+      <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {recipe.name}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            <TextTruncate line={3} element="span" truncateText="..." text={recipe.description}/>
+          </Typography>
+      </CardContent>
+      </CardActionArea>
+    </Card>
+  )
+  /*return(
     <Card key={recipe.id} style={{display:'flex', flexDirection:'column', justifyContent:'stretch', alignItems:'stretch', flexGrow:1}}>
       <CardContent style={{flexGrow:1}}>
         <Link to={`/recipe/${recipe.id}`}>
@@ -27,7 +57,7 @@ const RecipeCard = ({recipe}) => {
         <Button>View</Button>
       </CardActions>
     </Card>
-  )
+  )*/
 }
 
 export default RecipeCard;
